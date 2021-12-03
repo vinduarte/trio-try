9419bbeac5074cf295b9616b61b2293851616b20
Vinícius Duarte Lopes <viniciusduarte@ufv.br>
2021-12-03 15:28:08 -0300

    Adds the start of the real sync method

--- src/main/java/com/viniciusduartelopes/triotry/rest/v1/ContactsController.java
+++ src/main/java/com/viniciusduartelopes/triotry/rest/v1/ContactsController.java
@@ -16,6 +16,6 @@ public class ContactsController {
 
     @PostMapping("sync")
     public SyncResponseModel sync() {
-        return syncService.mockSync();
+        return syncService.sync();
     }
 }
--- src/main/java/com/viniciusduartelopes/triotry/rest/v1/configuration/service/SyncService.java
+++ src/main/java/com/viniciusduartelopes/triotry/rest/v1/configuration/service/SyncService.java
@@ -2,11 +2,20 @@ package com.viniciusduartelopes.triotry.rest.v1.configuration.service;
 
 import com.viniciusduartelopes.triotry.rest.v1.model.ContactModel;
 import com.viniciusduartelopes.triotry.rest.v1.model.SyncResponseModel;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Log
 public class SyncService {
+
+    @Autowired
+    private MockIOService mockIOService;
 
     public SyncResponseModel mockSync() {
         ContactModel mock1 = new ContactModel("my_mail01@gmail.com", "My First Name", "Last Name 1");
@@ -15,5 +24,21 @@ public class SyncService {
         syncModel.setSyncedContacts(2);
         syncModel.setContacts(Arrays.asList(mock1, mock2));
         return syncModel;
+    }
+
+    public SyncResponseModel sync() {
+        SyncResponseModel syncResponse = new SyncResponseModel(0, new ArrayList<>());
+
+        List<ContactModel> contacts;
+
+        try {
+            contacts = mockIOService.getContacts();
+        } catch (Exception ex) {
+            log.log(Level.INFO, "Error getting contacts from MockIO API");
+            return syncResponse;
+        }
+
+        return syncResponse;
+
     }
 }
